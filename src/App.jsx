641cdf6cf7bb5081df86ab9240afad963401493e
jhhg04 <jhhg04@gmail.com>
2022-05-27 22:31:10 -0500

    add User Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
-import User from './components/User/User';
+import Users from './components/Users/Users';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/user' element={<User />} />
+        <Route path='/users' element={<Users />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,27 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from '../../axios';
+import User from '../User/User';
 import './Home.css';
 
 const Home = () => {
+  const [query, setQuery] = useState('');
+  const [users, setUsers] = useState([]);
+  const handleQueryInput = (e) => {
+    const value = e.target.value;
+    setQuery(value);
+  };
+  const fetchUsers = async () => {
+    try {
+      const { data } = await axios.get('/search/users?q=' + query);
+      return data?.items;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
+  const handleSearchUsers = async (e) => {
+    e.preventDefault();
+    if (query) {
+      const items = await fetchUsers();
+      setUsers(items);
+    } else {
+      console.log('Your quuery is empty...');
+    }
+  };
   return (
     <div className='container'>
       <div className='search-form'>
         <h2>Github Search User</h2>
         <form>
-          <input type='text' />
-          <button>Search</button>
+          <input value={query} onChange={handleQueryInput} type='text' />
+          <button onClick={handleSearchUsers}>Search</button>
         </form>
       </div>
       <div className='search-results'>
-        <div className='user'>
-          <div className='image'>
-            <img src='https://avatars.githubusercontent.com/u/48237560?v=4' />
-          </div>
-          <div className='user-info'>
-            <h3>Name User</h3>
-            <small>ID</small>
-            <a href=''>View</a>
+        <div className='more-options'>
+          <label>
+            <small>Per Page</small>
+            <select>
+              <option value='10'>10</option>
+              <option value='20'>20</option>
+              <option value='50'>50</option>
+              <option value='100'>100</option>
+            </select>
+          </label>
+          <div className='pagination'>
+            <button>1</button>
+            <button>2</button>
           </div>
         </div>
+        {users ? (
+          users.map((user) => {
+            return <User user={user} key={user.id} />;
+          })
+        ) : (
+          <h2>There is nothing to display....</h2>
+        )}
       </div>
     </div>
   );
